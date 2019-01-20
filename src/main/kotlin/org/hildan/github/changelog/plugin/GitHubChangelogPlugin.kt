333fdc166a16c195031702818808bb1b86168d1c
Joffrey BION <joffrey.bion@gmail.com>
2019-01-20 12:06:19 +0100

    Flatten plugin extension to fix DSL issues

---
+++ src/main/kotlin/org/hildan/github/changelog/plugin/GitHubChangelogPlugin.kt
@@ -0,0 +1,84 @@
+package org.hildan.github.changelog.plugin
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.GradleException
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.TaskAction
+import org.hildan.github.changelog.generator.Configuration
+import org.hildan.github.changelog.generator.DEFAULT_SECTIONS
+import org.hildan.github.changelog.generator.GitHubConfig
+import org.hildan.github.changelog.generator.GithubChangelogGenerator
+import org.hildan.github.changelog.generator.SectionDefinition
+import java.io.File
+import javax.inject.Inject
+
+open class GithubChangelogPlugin : Plugin<Project> {
+
+    override fun apply(project: Project) {
+        val ext = project.extensions.create("changelog", GitHubChangelogExtension::class.java, project)
+        project.tasks.create("generateChangelog", GenerateChangelogTask::class.java, ext)
+    }
+}
+
+open class GenerateChangelogTask @Inject constructor(
+    private val ext: GitHubChangelogExtension
+) : DefaultTask() {
+
+    init {
+        group = "documentation"
+        description = "Generates the changelog of the project based on GitHub tags, issues and pull-requests."
+    }
+
+    @TaskAction
+    fun generate() {
+        val configuration = ext.toConfig()
+        val generator = GithubChangelogGenerator(configuration, ext.outputFile)
+        generator.generate()
+    }
+}
+
+open class GitHubChangelogExtension(project: Project) {
+
+    var githubUser: String? = project.getPropOrEnv("githubUser", "GITHUB_USER")
+    var githubToken: String? = project.getPropOrEnv("githubToken", "GITHUB_TOKEN")
+    var githubRepository: String = project.name
+
+    var title: String = "Changelog"
+    var unreleasedVersionTitle: String = "Unreleased"
+    val showUnreleased: Boolean = true
+    val futureVersion: String? = project.version.toString()
+    val sections: List<SectionDefinition> =
+        DEFAULT_SECTIONS
+    val defaultIssueSectionTitle: String = "Closed issue:"
+    val defaultPrSectionTitle: String = "Merged pull requests:"
+    val includeLabels: List<String> = emptyList()
+    val excludeLabels: List<String> = listOf("duplicate", "invalid", "question", "wontfix")
+
+    var outputFile: File = File("${project.projectDir}/CHANGELOG.md")
+
+    fun toConfig(): Configuration =
+        Configuration(
+            github = GitHubConfig(
+                githubUser ?: throw GradleException("you must specify your github username for changelog generation"),
+                githubToken,
+                githubRepository
+            ),
+            globalHeader = title,
+            unreleasedTitle = unreleasedVersionTitle,
+            futureVersion = futureVersion,
+            sections = sections,
+            showUnreleased = showUnreleased,
+            defaultIssueSectionTitle = defaultIssueSectionTitle,
+            defaultPrSectionTitle = defaultPrSectionTitle,
+            includeLabels = includeLabels,
+            excludeLabels = excludeLabels
+        )
+}
+
+private fun Project.getPropOrEnv(propName: String, envVar: String? = null, defaultValue: String? = null): String? =
+    if (hasProperty(propName)) {
+        property(propName) as String
+    } else {
+        System.getenv(envVar) ?: defaultValue
+    }
---
+++ src/test/kotlin/org/hildan/github/changelog/plugin/GitHubChangelogPluginTest.kt
@@ -0,0 +1,63 @@
+package org.hildan.github.changelog.plugin
+
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+
+class GitHubChangelogPluginTest {
+    @get:Rule
+    val testProjectDir = TemporaryFolder()
+
+    lateinit var project: Project
+
+    @Before
+    fun setUp() {
+        project = ProjectBuilder.builder().build()
+        project.pluginManager.apply(GithubChangelogPlugin::class.java)
+    }
+
+    @Test
+    fun `creates generateChangelog task`() {
+        val changelogTask = project.tasks.getByName("generateChangelog")
+        assertNotNull(changelogTask)
+    }
+
+    @Test
+    fun `creates changelog extension`() {
+        val changelogExt = project.extensions.getByName("changelog")
+        assertNotNull(changelogExt)
+    }
+
+    @Test
+    fun `generateChangelog task creates changelog file`() {
+        val buildFile = testProjectDir.newFile("build.gradle")
+        buildFile.writeText("""
+            plugins {
+                id 'org.hildan.github.changelog.generator'
+            }
+
+            changelog {
+                title = "My Change Log"
+                githubUser = "joffrey-bion"
+                githubRepository = "fx-gson"
+            }
+        """.trimIndent())
+
+        GradleRunner.create()
+            .withProjectDir(testProjectDir.root)
+            .withArguments("generateChangelog")
+            .withPluginClasspath()
+            .build()
+
+        val files = testProjectDir.root.listFiles()
+        val changelogFile = files.firstOrNull { it.name == "CHANGELOG.md" }
+        assertNotNull(changelogFile)
+        assertTrue(changelogFile!!.readText().startsWith("# My Change Log"))
+    }
+}
