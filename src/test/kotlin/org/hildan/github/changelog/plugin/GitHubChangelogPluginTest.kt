e7fcd30dd21a08962d161fb0293b7ca9277b4f9d
Joffrey BION <joffrey.bion@gmail.com>
2019-01-26 01:09:06 +0100

    Remove unstable tests

--- src/test/kotlin/org/hildan/github/changelog/plugin/GitHubChangelogPluginTest.kt
+++ src/test/kotlin/org/hildan/github/changelog/plugin/GitHubChangelogPluginTest.kt
@@ -1,16 +1,10 @@
 package org.hildan.github.changelog.plugin
 
 import org.gradle.testfixtures.ProjectBuilder
-import org.gradle.testkit.runner.GradleRunner
 import org.junit.Assert.assertNotNull
-import org.junit.Assert.assertTrue
-import org.junit.Rule
 import org.junit.Test
-import org.junit.rules.TemporaryFolder
 
 class GitHubChangelogPluginTest {
-    @get:Rule
-    val testProjectDir = TemporaryFolder()
 
     @Test
     fun `creates generateChangelog task`() {
@@ -26,61 +20,5 @@ class GitHubChangelogPluginTest {
         project.pluginManager.apply(GitHubChangelogPlugin::class.java)
         val changelogExt = project.extensions.getByName("changelog")
         assertNotNull(changelogExt)
-    }
-
-    @Test
-    fun `generateChangelog task creates changelog file`() {
-        val buildFile = testProjectDir.newFile("build.gradle")
-        buildFile.writeText("""
-            plugins {
-                id 'org.hildan.github.changelog'
-            }
-
-            changelog {
-                githubUser = "joffrey-bion"
-                githubRepository = "fx-gson"
-            }
-        """.trimIndent())
-
-        GradleRunner.create()
-            .withProjectDir(testProjectDir.root)
-            .withArguments("generateChangelog")
-            .withPluginClasspath()
-            .build()
-
-        val files = testProjectDir.root.listFiles()
-        val changelogFile = files.firstOrNull { it.name == "CHANGELOG.md" }
-        assertNotNull(changelogFile)
-        assertTrue(changelogFile!!.readText().startsWith("# Changelog"))
-    }
-
-    @Test
-    fun `generateChangelog task creates custom changelog file`() {
-        val buildFile = testProjectDir.newFile("build.gradle")
-        buildFile.writeText("""
-            plugins {
-                id 'org.hildan.github.changelog'
-            }
-
-            changelog {
-                title = "My Change Log"
-                githubUser = "joffrey-bion"
-                githubRepository = "fx-gson"
-                releaseUrlTemplate = "https://my.server/releases/%s"
-            }
-        """.trimIndent())
-
-        GradleRunner.create()
-            .withProjectDir(testProjectDir.root)
-            .withArguments("generateChangelog")
-            .withPluginClasspath()
-            .build()
-
-        val files = testProjectDir.root.listFiles()
-        val changelogFile = files.firstOrNull { it.name == "CHANGELOG.md" }
-        assertNotNull(changelogFile)
-        val changelogText = changelogFile!!.readText()
-        assertTrue(changelogText.startsWith("# My Change Log"))
-        assertTrue(changelogText.contains("https://my.server/releases/v1.2"))
     }
 }
