dfdb78b3842220e5eebca7ff170da048f65e75f0
Joffrey Bion <joffrey.bion@gmail.com>
2021-12-30 01:02:37 +0100

    Upgrade changelog generator plugin in our own build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     `kotlin-dsl`
     id("com.gradle.plugin-publish") version "0.12.0"
-    id("org.hildan.github.changelog") version "1.3.0"
+    id("org.hildan.github.changelog") version "1.8.0"
 }
 
 group = "org.hildan.gradle"
