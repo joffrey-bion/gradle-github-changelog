ac8c28d2f9134e48aa0f355bc67b1034b8907d6f
Joffrey Bion <joffrey.bion@gmail.com>
2022-01-10 02:20:01 +0100

    Add main function for manual tests

---
+++ src/test/kotlin/org/hildan/github/changelog/ManualTests.kt
@@ -0,0 +1,21 @@
+package org.hildan.github.changelog
+
+import org.hildan.github.changelog.builder.ChangelogConfig
+import org.hildan.github.changelog.github.GitHubConfig
+import java.io.File
+
+fun main() {
+    val gitHubConfig = GitHubConfig(
+        user = "joffrey-bion",
+        repo = "github-changelog-generator",
+        token = System.getenv("GITHUB_TOKEN"),
+    )
+    val config = GitHubChangelogGeneratorConfig(
+        gitHubConfig,
+        ChangelogConfig(
+            releaseUrlTemplate = gitHubConfig.releaseUrlTemplate,
+            diffUrlTemplate = gitHubConfig.diffUrlTemplate,
+        ),
+    )
+    GitHubChangeLogGenerator(config).generate(File("CHANGELOG.md"))
+}
