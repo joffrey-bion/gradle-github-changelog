dbefdbd3fee08b4a1d06023496590430a86b55e4
Joffrey Bion <joffrey.bion@gmail.com>
2022-11-01 01:57:38 +0100

    Fix project name in manual test

--- src/test/kotlin/org/hildan/github/changelog/ManualTests.kt
+++ src/test/kotlin/org/hildan/github/changelog/ManualTests.kt
@@ -7,7 +7,7 @@ import java.io.File
 fun main() {
     val gitHubConfig = GitHubConfig(
         user = "joffrey-bion",
-        repo = "github-changelog-generator",
+        repo = "gradle-github-changelog",
         token = System.getenv("GITHUB_TOKEN"),
     )
     val config = GitHubChangelogGeneratorConfig(
