c637b947b0dd01e1c9b84ade3a75965dc31c1cd1
Joffrey Bion <joffrey.bion@gmail.com>
2022-11-01 01:28:14 +0100

    Upgrade test dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,8 @@ dependencies {
 
     testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit5"))
-    testImplementation("io.mockk:mockk:1.12.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("io.mockk:mockk:1.13.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 }
 
 tasks.test {
